db10fbd013e6b8b9dceea0d076908c5ff4cb8fc6
Whitney Perricone <whitney.perricone@gmail.com>
2022-02-24 11:54:07 -0700

    Add Palette Class

--- color.js
+++ color.js
@@ -1,39 +1,54 @@
-var hexPlacement = document.querySelectorAll('.hex');
-var buttonNewPalette = document.querySelector('#newPalette');
-buttonNewPalette.addEventListener('click', createHex);
+
 
 class Color {
   constructor(output) {
-  this.hexPlacement = output;
+  this.hexCode = output;
   this.locked = false;
   }
 
-
-
-  getRandomHex(hexValues) {
-    var hexValues = [
-      'A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9'
-    ];
-    var output = '#'
-    for (var i = 0; i < 6; i++) {
+getRandomHex(hexValues) {
+  var hexValues = [
+    'A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9'
+  ];
+  var output = '#'
+  for (var i = 0; i < 6; i++) {
     output += hexValues[Math.floor(Math.random() * hexValues.length)];
-    }
-
-    // // title.innerText = titles[getRandomIndex(titles)];
-    // // tagline1.innerText = descriptors[getRandomIndex(descriptors)];
-    console.log(this.hexPlacement)
-    return this.hexPlacement
-    console.log(output)
-    // // hexPlacement.innerText = hexValues[getRandomHex(hexValues)]
-
+  }
+  console.log(output)
+  return output;
 }
 }
-function createHex(output) {
-  hexPlacement.innerText = output;
-}
+
+// function createHex(output) {
+//    hexPlacement.innerText = output;
+// //   //currentPalette = new Palette()
+ //}
 
 
-  // function getRandomHex() {
-  //   var randHex = hexValues[getRandomIndex(hexValues)]
-  //   return
-  // }
+//   getRandomHex(hexValues) {
+//     var hexValues = [
+//       'A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9'
+//     ];
+//     var output = '#'
+//     for (var i = 0; i < 6; i++) {
+//     output += hexValues[Math.floor(Math.random() * hexValues.length)];
+//     }
+//
+//     // // title.innerText = titles[getRandomIndex(titles)];
+//     // // tagline1.innerText = descriptors[getRandomIndex(descriptors)];
+//     console.log(this.hexPlacement)
+//     return this.hexPlacement
+//     console.log(output)
+//     // // hexPlacement.innerText = hexValues[getRandomHex(hexValues)]
+//
+// }
+// }
+// function createHex(output) {
+//   hexPlacement.innerHTML = output;
+// }
+//
+//
+//   // function getRandomHex() {
+//   //   var randHex = hexValues[getRandomIndex(hexValues)]
+//   //   return
+//   // }
--- main.js
+++ main.js
@@ -1,3 +1,35 @@
-function createHex(output) {
-  hexPlacement.innerText = output;
+var hexPlacement = document.querySelectorAll('.hex');
+var buttonNewPalette = document.querySelector('#newPalette');
+buttonNewPalette.addEventListener('click', newPalette);
+
+function newPalette() {
+
 }
+// function getRandomHex(hexValues) {
+//   var hexValues = [
+//     'A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9'
+//   ];
+//   var output = '#'
+//   for (var i = 0; i < 6; i++) {
+//   output += hexValues[Math.floor(Math.random() * hexValues.length)];
+//   }
+//   return output
+// }
+  // // title.innerText = titles[getRandomIndex(titles)];
+  // // tagline1.innerText = descriptors[getRandomIndex(descriptors)];
+  // console.log(this.hexPlacement)
+
+  // console.log(output)
+  // // hexPlacement.innerText = hexValues[getRandomHex(hexValues)]
+
+
+// function createHex(output) {
+//   hexPlacement.innerText = output;
+//   //currentPalette = new Palette()
+// }
+
+
+// function getRandomHex() {
+//   var randHex = hexValues[getRandomIndex(hexValues)]
+//   return
+// }
