291ca4c84adf7c7bed78bd9df46122fdb52191fa
Grant Beard <Grantxbeard@gmail.com>
2022-02-24 09:22:15 -0700

    Update color file

--- color.js
+++ color.js
@@ -1,16 +1,16 @@
+var hexPlacement = document.querySelectorAll('.hex');
+var buttonNewPalette = document.querySelector('#newPalette');
+buttonNewPalette.addEventListener('click', createHex);
+
 class Color {
-  constructor() {
-  this.hexCode = ;
+  constructor(output) {
+  this.hexPlacement = output;
   this.locked = false;
   }
-}
 
-  var hexPlacement = document.querySelectorAll('.hex');
-  var buttonNewPalette = document.querySelector('#newPalette');
-  buttonNewPalette.addEventListener('click', getRandomHex);
 
-  // Functions
-  function getRandomHex(hexValues) {
+
+  getRandomHex(hexValues) {
     var hexValues = [
       'A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9'
     ];
@@ -18,10 +18,21 @@ class Color {
     for (var i = 0; i < 6; i++) {
     output += hexValues[Math.floor(Math.random() * hexValues.length)];
     }
-    console.log(output)
-    return output
 
-  }
+    // // title.innerText = titles[getRandomIndex(titles)];
+    // // tagline1.innerText = descriptors[getRandomIndex(descriptors)];
+    console.log(this.hexPlacement)
+    return this.hexPlacement
+    console.log(output)
+    // // hexPlacement.innerText = hexValues[getRandomHex(hexValues)]
+
+}
+}
+function createHex(output) {
+  hexPlacement.innerText = output;
+}
+
+
   // function getRandomHex() {
   //   var randHex = hexValues[getRandomIndex(hexValues)]
   //   return
