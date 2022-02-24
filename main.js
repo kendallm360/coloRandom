bcab7db095f8a8b78a5dd3dfe68cab2ba2ccc275
Grant Beard <Grantxbeard@gmail.com>
2022-02-23 20:47:38 -0700

    Add color class

---
+++ main.js
@@ -0,0 +1,21 @@
+var hexPlacement = document.querySelectorAll('.hex');
+var buttonNewPalette = document.querySelector('#newPalette');
+buttonNewPalette.addEventListener('click', getRandomHex);
+
+// Functions
+function getRandomHex(hexValues) {
+  var hexValues = [
+    'A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9'
+  ];
+  var output = '#'
+  for (var i = 0; i < 6; i++) {
+  output += hexValues[Math.floor(Math.random() * hexValues.length)];
+  }
+  console.log(output)
+  return output
+
+}
+// function getRandomHex() {
+//   var randHex = hexValues[getRandomIndex(hexValues)]
+//   return
+// }
