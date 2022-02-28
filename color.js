16dba5c118df3f06b89f9555bda67ab8de24008b
Grant Beard <Grantxbeard@gmail.com>
2022-02-28 14:42:25 -0700

    Revise HTML and Javascript

--- color.js
+++ color.js
@@ -4,25 +4,9 @@ class Color {
     this.locked = false;
     this.id = Date.now();
   }
-
   getRandomHex() {
     var hexValues = [
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-      "0",
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
+      "A", "B", "C", "D", "E", "F", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
     ];
     var output = "#";
     for (var i = 0; i < 6; i++) {
@@ -30,4 +14,4 @@ class Color {
     }
     return output;
   }
-}
+};
